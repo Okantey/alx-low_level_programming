a8265d9a68ec74d8b402f7117d200f349f3f0c21
nii_okantey <nunoogideon19@gmail.com>
2023-04-10 19:17:36 +0000

    updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,68 +1,60 @@
 #include "main.h"
 
 /**
-  * _strlen - length of a string
-  * @s: input char
-  * Return: length of a string
-**/
+ * _strlen - returns the length of a string
+ * @s: pointer to input string
+ *
+ * Return: length of the string
+ */
 
 int _strlen(char *s)
 {
-	int i = 0;
+    int len = 0;
 
-	while (s[i])
-	{
-		i++;
+    while (s[len] != '\0')
+    {
+        len++;
+    }
 
-	}
-
-	return (i);
+    return (len);
 }
 
 /**
-* create_file - check the code for Holberton School students.
-* @filename: file to create.
-* @text_content: info to write into the file.
-* Return: 1 on success, -1 on failure
-*/
+ * create_file - creates a file with the given filename and writes the given
+ * text content to it
+ * @filename: pointer to the filename string
+ * @text_content: pointer to the text content string
+ *
+ * Return: 1 on success, -1 on failure
+ */
 
 int create_file(const char *filename, char *text_content)
-
 {
-	ssize_t nletters;
+    int fd, res;
+    ssize_t len;
 
-	int file;
+    if (!filename)
+        return (-1);
 
-	if (!filename)
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-		return (1);
+    if (fd == -1)
+        return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (text_content)
+    {
+        len = _strlen(text_content);
+        res = write(fd, text_content, len);
 
-	if (file == -1)
+        if (res == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	{
+    close(fd);
 
-		return (-1);
-
-	}
-
-	if (text_content)
-
-	{
-
-		nletters = write(file, text_content, _strlen(text_content));
-
-		if (nletters == -1)
-
-		{
-			close(file);
-
-			return (-1);
-
-		}
-	}
-
-	close(file);
-	return (1);
+    return (1);
 }
+
